f03908c980f28972f669a4359b4ec21549de0874
xujif <i@xujif.com>
2016-05-05 11:43:03 +0800

    修改

--- start-nginx.sh
+++ start-nginx.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-sed -i 's#\$DOCUMENT_ROOT#'"$DOCUMENT_ROOT"'/#' /etc/nginx/nginx.conf
-nginx -g "daemon off;" 
+sed 's#\$DOCUMENT_ROOT#'"$DOCUMENT_ROOT"'/#' /etc/nginx/nginx.conf > /etc/nginx/real_nginx.conf
+nginx -g "daemon off;" -c /etc/nginx/real_nginx.conf
