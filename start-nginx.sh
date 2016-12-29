7bd19bc45ffeb2c8cd64e2fa2502705efce7daa1
xujif <i@xujif.com>
2016-12-29 12:09:00 +0800

    'fix'

--- start-nginx.sh
+++ start-nginx.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 sed 's#\$DOCUMENT_ROOT#'"$DOCUMENT_ROOT"'/#' /etc/nginx/nginx.conf > /etc/nginx/real_nginx.conf
-sed 's#\$FASTCGI_HTTPS#'"$FASTCGI_HTTPS"'/#' /etc/nginx/nginx.conf > /etc/nginx/real_nginx.conf
+sed 's#\$FASTCGI_HTTPS#'"$FASTCGI_HTTPS"'#' /etc/nginx/nginx.conf > /etc/nginx/real_nginx.conf
 nginx -g "daemon off;" -c /etc/nginx/real_nginx.conf
