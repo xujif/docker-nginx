a69e9ff65f34534afe69737aaef8f28a210d836c
xujif <i@xujif.com>
2017-02-04 12:52:37 +0800

    修改

--- start-nginx.sh
+++ start-nginx.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-sed -i 's#\$DOCUMENT_ROOT#'"$DOCUMENT_ROOT"'/#' /etc/nginx/nginx.conf
-sed -i 's#\$FASTCGI_HTTPS#'"$FASTCGI_HTTPS"'#' /etc/nginx/nginx.conf
-nginx -g "daemon off;" -c /etc/nginx/nginx.conf
+cp /etc/nginx/nginx.conf /tmp/real_nginx.conf
+sed -i 's#\$DOCUMENT_ROOT#'"$DOCUMENT_ROOT"'/#' /tmp/real_nginx.conf
+sed -i 's#\$FASTCGI_HTTPS#'"$FASTCGI_HTTPS"'#' /tmp/real_nginx.conf
+nginx -g "daemon off;" -c /tmp/real_nginx.conf
