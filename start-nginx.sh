0de8402287230e845ce65272bc7509fc75f0eeda
xujif <i@xujif.com>
2017-02-07 18:25:24 +0800

    fix bug

--- start-nginx.sh
+++ start-nginx.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-cp /etc/nginx/nginx.conf /tmp/real_nginx.conf
-sed -i 's#\$DOCUMENT_ROOT#'"$DOCUMENT_ROOT"'/#' /tmp/real_nginx.conf
-sed -i 's#\$FASTCGI_HTTPS#'"$FASTCGI_HTTPS"'#' /tmp/real_nginx.conf
-nginx -g "daemon off;" -c /tmp/real_nginx.conf
+cp /etc/nginx/nginx.conf /etc/nginx/real_nginx.conf
+sed -i 's#\$DOCUMENT_ROOT#'"$DOCUMENT_ROOT"'/#' /etc/nginx/real_nginx.conf
+sed -i 's#\$FASTCGI_HTTPS#'"$FASTCGI_HTTPS"'#' /etc/nginx/real_nginx.conf
+nginx -g "daemon off;" -c /etc/nginx/real_nginx.conf
