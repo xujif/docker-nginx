f47225d49523e6077d611ec8dc15a841426589fc
xujif <i@xujif.com>
2017-07-03 15:58:03 +0800

    修改

--- reverse/start-nginx.sh
+++ reverse/start-nginx.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-sed 's#\$BACKEND#'"$BACKEND"'/#' /etc/nginx/nginx.conf > /tmp/real_nginx.conf
+sed 's#\$BACKEND#'"$BACKEND"'#' /etc/nginx/nginx.conf | sed 's#\$PROXY_HOST#'"$PROXY_HOST"'#' >/tmp/real_nginx.conf
 nginx -g "daemon off;" -c /tmp/real_nginx.conf
