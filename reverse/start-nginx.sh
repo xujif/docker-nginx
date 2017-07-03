f5e06ace2edbb7255021bd8e086436971a0da518
xujif <i@xujif.com>
2017-07-03 15:29:16 +0800

    修改

--- reverse/start-nginx.sh
+++ reverse/start-nginx.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
-cp /etc/nginx/nginx.conf /etc/nginx/real_nginx.conf
-sed -i 's#\$DOCUMENT_ROOT#'"$DOCUMENT_ROOT"'/#' /etc/nginx/real_nginx.conf
-sed -i 's#\$FASTCGI_HTTPS#'"$FASTCGI_HTTPS"'#' /etc/nginx/real_nginx.conf
-nginx -g "daemon off;" -c /etc/nginx/real_nginx.conf
+sed 's#\$BACKEND#'"$BACKEND"'/#' /etc/nginx/nginx.conf > /tmp/real_nginx.conf
+nginx -g "daemon off;" -c /tmp/real_nginx.conf
